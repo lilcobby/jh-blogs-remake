e200c5cb7d0540776dfd30b318534cc932bcfb24
lilcobby <jshairfield13@gmail.com>
2023-11-30 23:42:46 -0500

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
-const Sequelize = require("sequelize");
-require("dotenv").config();
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
 let sequelize;
 
@@ -7,13 +7,13 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    (DB_NAME = "jhBlog_db"),
-    (DB_USER = "root"),
-    (DB_PASSWORD = "rootroot"),
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
-      host: "localhost",
-      dialect: "mysql",
-      port: 3306,
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
     }
   );
 }
