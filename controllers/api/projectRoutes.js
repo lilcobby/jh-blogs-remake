51f4884ea70cea6b1d363c497e06c2b705ea9cc9
lilcobby <jshairfield13@gmail.com>
2023-11-30 23:24:44 -0500

    so close just ran out of time to style and properly test put route, seems to work just isnt replaceing existing text

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -56,22 +56,19 @@ router.delete("/:id", withAuth, async (req, res) => {
 });
 // put route works just need front end html and js
 
-// router.put("/:id", withAuth, async (req, res) => {
-//   try {
-//     const updateProj = await Project.update(
-//       { name: req.body.name, description: req.body.description },
-//       {
-//         where: {
-//           id: req.params.id,
-//         },
-//       }
-//     );
-//     res.status(200).json(updateProj);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+router.put("/:id", withAuth, async (req, res) => {
+  try {
+    const updateProj = await Project.update(
+      { name: req.body.name, description: req.body.description },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.status(200).json(updateProj);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
-
-
-
--- public/js/comment.js
+++ public/js/comment.js
@@ -26,3 +26,27 @@ const newCommentHandler = async (event) => {
 document
   .querySelector(".new-comment-form")
   .addEventListener("submit", newCommentHandler);
+
+// put
+const newUpdate = async (event) => {
+  event.preventDefault();
+  const updVal = document.querySelector("#upd").value.trim();
+  const upd2 = document.querySelector("#upd2").value.trim();
+  if (updVal && upd2) {
+    const response = await fetch("/api/projects/:id", {
+      method: "PUT",
+      body: JSON.stringify({ updVal, upd2 }),
+      headers: { "Content-Type": "application/json" },
+    });
+    console.log(response);
+    // if (response.ok) {
+    //   await fetch("/api/comments", { method: "get" });
+    //   console.log(`it works`);
+    // } else {
+    //   console.log(response);
+    // }
+  } else {
+  }
+};
+
+document.querySelector(".new-put-form").addEventListener("submit", newUpdate);
